2448b2339723460362863a0e2f95b4629b38f2b5
Masaaki Nakata <masaaki.nakata@gmail.com>
2023-05-18 18:24:50 +0900

    2023/05/18/18:24 作業データ更新 周波数から音階への変換テスト、不具合の修正

--- Unity/SoundTest/Assets/script/DisplayAudioData.cs
+++ Unity/SoundTest/Assets/script/DisplayAudioData.cs
@@ -3,19 +3,25 @@ using UnityEngine.UI;
 
 public class DisplayAudioData : MonoBehaviour
 {
-    public Text gainText;
+    public Text loudness;
     public Text freqText;
+    public Text noteText; // 追加: 音階名を表示するテキスト
     private GetAudio audioSource;
+    private AnalyzePitch pitchAnalyzer; // 追加: AnalyzePitchコンポーネント
 
     void Start()
     {
         audioSource = GetComponent<GetAudio>();
+        pitchAnalyzer = GetComponent<AnalyzePitch>(); // 追加: AnalyzePitchコンポーネントを取得
     }
 
     void Update()
     {
         // ゲインと周波数をテキストとして表示
-        gainText.text = "Gain: " + audioSource.gain.ToString("F2");
+        loudness.text = "Loudness: " + audioSource.loudness.ToString("F2");
         freqText.text = "Frequency: " + audioSource.freq.ToString("F2") + " Hz";
+
+        // 音階名をテキストとして表示
+        noteText.text = "Note: " + pitchAnalyzer.currentNote;
     }
 }
