99c409bca9e439d1a75ebe44325adc2d0f923a1c
Masaaki Nakata <masaaki.nakata@gmail.com>
2023-06-08 18:23:42 +0900

    2023/06/08/18:23 作業データ更新 波形のお変更を実装

--- Unity/SoundTest/Assets/script/DynamicAudioGeneration.cs
+++ Unity/SoundTest/Assets/script/DynamicAudioGeneration.cs
@@ -6,6 +6,8 @@ public class DynamicAudioGeneration : MonoBehaviour
     public float gain = 0.1f; // ゲイン（音量）の設定
     public float sampleRate = 48000f; // サンプルレート
     public float noteChangeInterval = 1f; // 音の切り替え間隔
+    [SerializeField, Range(1,4)]
+    public int PlayState = 1;
 
     private float phase; // sin波の位相
 
@@ -44,15 +46,21 @@ public class DynamicAudioGeneration : MonoBehaviour
         }
     }
 
-    void OnAudioFilterRead(float[] data, int channels)
+    void SineWave(float[] data, int channels)
     {
-        if (!isPlaying)
+        increment = frequency * 4 * Mathf.PI / sampleRate;
+        for (var i = 0; i < data.Length; i = i + channels)
         {
-            return;
+            time = time + increment;
+            data[i] = Mathf.Sin(time);
+            if (channels == 2) data[i + 1] = data[i];
+            if (time > 2 * Mathf.PI) time = 0;
         }
+    }
 
-        increment = frequency * 2 * Mathf.PI / sampleRate;
-
+    void SquareWave(float[] data, int channels)
+    {
+        increment = frequency * 4 * Mathf.PI / sampleRate;
         for (var i = 0; i < data.Length; i = i + channels)
         {
             time = time + increment;
@@ -60,6 +68,56 @@ public class DynamicAudioGeneration : MonoBehaviour
             if (time > 0 && time <= Mathf.PI) data[i] = 1;
             if (channels == 2) data[i + 1] = data[i];
             if (time > 2 * Mathf.PI) time = 0;
+        }
+    }
+
+    void TriangleWave(float[] data, int channels)
+    {
+        increment = frequency * 4 * Mathf.PI / sampleRate;
+        for (var i = 0; i < data.Length; i = i + channels)
+        {
+            time = time + increment;
+            if (time > -Mathf.PI && time <= -Mathf.PI / 2) data[i] = -2 * (time + Mathf.PI);
+            if (time > -Mathf.PI / 2 && time <= Mathf.PI / 2) data[i] = 2 * time;
+            if (time > Mathf.PI / 2 && time <= Mathf.PI) data[i] = -2 * (time - Mathf.PI);
+            if (channels == 2) data[i + 1] = data[i];
+            if (time > 2 * Mathf.PI) time = 0;
+        }
+    }
+
+    void SawtoothWave(float[] data, int channels)
+    {
+        increment = frequency * 4 * Mathf.PI / sampleRate;
+        for (var i = 0; i < data.Length; i = i + channels)
+        {
+            time = time + increment;
+            data[i] = (float)(0.5 * ((time + Mathf.PI) % (Mathf.PI * 2)) / Mathf.PI - 1.0);
+            if (channels == 2) data[i + 1] = data[i];
+            if (time > 2 * Mathf.PI) time = 0;
+        }
+    }
+
+    void OnAudioFilterRead(float[] data, int channels)
+    {
+        if (!isPlaying)
+        {
+            return;
+        }
+
+        switch (PlayState)
+        {
+            case 1:
+                SineWave(data, channels);
+                break;
+            case 2:
+                SquareWave(data, channels);
+                break;
+            case 3:
+                TriangleWave(data, channels);
+                break;
+            case 4:
+                SawtoothWave(data, channels);
+                break;
         }
     }
 
